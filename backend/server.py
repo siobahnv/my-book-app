30d93ad5a27a00978170c17e43166025017fcf5f
siobahnv <siobahnv@gmail.com>
2018-12-15 10:50:12 -0800

    Got a value to display

--- backend/server.py
+++ backend/server.py
@@ -4,19 +4,49 @@ from model import User, Book, BookList, connect_to_db, db
 
 from secrets import *
 
-from flask import (Flask, render_template, redirect, request, flash,
+from flask import (Flask, make_response, render_template, redirect, request, flash,
                    session, jsonify, url_for)
+
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = my_secret_key
 
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 @app.route("/")
 def index():
   """Homepage."""
+  session['user_id'] = 1 # fix later
 
-  return render_template("index.html")
+  if session.get('user_id'):
+    return "homepage"
+  else:
+    return "login"
+
+@app.route("/booklist")
+@cross_origin()
+def booklist():
+  """Returns booklist."""
+  
+  # TODO: get "user" from session; for phase 0/1 store booklist_id as well as user_id?
+  user = User.query.filter(User.user_id==1).one()
+  mybooklist = BookList.query.filter(BookList.user_id==user.user_id).all()
+  books = Book.query.filter(Book.booklist_id==mybooklist[0].booklist_id).all()
+
+  print(books)
+
+  # response = make_response(jsonify({'book1': 1, 'book2': 2}))
+  # response.headers['Access-Control-Allow-Origin'] = '*' # fine for testing...
+  # return response
+  return jsonify({'book1': 1, 'book2': 2})
+
+@app.route("/book/<book_id>")
+def get_book(book_id):
+  """Returns a book."""
+  pass
 
 if __name__ == "__main__":
     
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './static/index.css';
 
 import App from './js/App';
-import SearchComponent from './js/Search';
+// import SearchComponent from './js/Search';
 
 import * as serviceWorker from './js/serviceWorker';
 
-ReactDOM.render(
-    ( < React.Fragment > 
-        <App/> 
-        <SearchComponent/>
-        </React.Fragment>
-    ),
-    document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
