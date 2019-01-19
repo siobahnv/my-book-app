261e7980cc1b3978b8e96b2fd4deb8b81b945dd9
siobahnv <siobahnv@gmail.com>
2019-01-19 10:05:07 -0800

    Install redux dev tools

--- frontend/src/js/reducers/index.js
+++ frontend/src/js/reducers/index.js
@@ -13,14 +13,16 @@ function rootReducer(state = initialState, action) {
     if (action.type === actions.FETCH_BOOKS) {
         return Object.assign({}, state, {
             ...state,
-            bookresults: state.bookresults.concat(action.payload)
+            bookresults: action.payload
+            // bookresults: state.bookresults.concat(action.payload)
         });
     }
 
     if (action.type === actions.FETCH_BOOKLIST) {
         return Object.assign({}, state, {
             ...state,
-            booklist: state.booklist.concat(action.payload)
+            booklist: action.payload
+            // booklist: state.booklist.concat(action.payload)
         });
       }
 
