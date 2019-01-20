cbb4f1b26fb2441b5470861e68c29fc6aad9da38
siobahnv <siobahnv@gmail.com>
2019-01-20 00:15:07 -0800

    Use Dan's localStorage technique instead of redux-persist.

--- frontend/src/js/store/store.js
+++ frontend/src/js/store/store.js
@@ -2,7 +2,17 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/index";
+import { loadState, saveState } from "./localStorage";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistedState = loadState();
+const store = createStore(
+    rootReducer, 
+    persistedState,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 export default store;
