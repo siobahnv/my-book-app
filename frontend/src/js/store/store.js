e4a3ff208bc26246ec0b296c16fd2d39482816c4
siobahnv <siobahnv@gmail.com>
2019-01-20 00:19:42 -0800

    Finish Dan's strategy.

--- frontend/src/js/store/store.js
+++ frontend/src/js/store/store.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/index";
 import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 
 const persistedState = loadState();
 const store = createStore(
@@ -11,8 +12,8 @@ const store = createStore(
     composeWithDevTools(applyMiddleware(thunk))
 );
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
     saveState(store.getState());
-});
+}, 1000));
 
 export default store;
